2b08f25b404e8aa7ccd8fd7b57802ed14f5e9f03
thatsShirleyLee <2690233316@qq.com>
2025-08-24 18:27:02 +0800

    feat: make test 'should_move_forward_one_step_when_facing_north' pass

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -12,4 +12,21 @@ public class MarsRover {
     public String report() {
         return "(" + x + ", " + y + ") " + direction;
     }
+
+    public void execute(String commands) {
+        for (char c : commands.toCharArray()) {
+            if (c == 'M') {
+                moveForward();
+            }
+        }
+    }
+
+    private void moveForward() {
+        switch (direction) {
+            case N -> y++;
+            case E -> x++;
+            case S -> y--;
+            case W -> x--;
+        }
+    }
 }
