31f1e262e0005b69ab9d0f3d184143b6e93a5558
thatsShirleyLee <2690233316@qq.com>
2025-08-24 18:25:49 +0800

    test: should_move_forward_one_step_when_facing_north

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -4,11 +4,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MarsRoverTest {
     @Test
-    void should_report_given_rover_when_init() {
+    void should_report_initial_position_and_direction() {
         // Given
         MarsRover rover = new MarsRover(0, 0, Direction.N);
         // When & Then
         String report = rover.report();
         assertEquals("(0, 0) N", report);
+    }
+    @Test
+    void should_move_forward_one_step_when_facing_north() {
+        // Given
+        MarsRover rover = new MarsRover(0, 0, Direction.N);
+        // When
+        rover.execute("M");
+        // Then
+        assertEquals("(0, 1) N", rover.report());
     }
 }
